951c1c972d1015982ce825bf9ab08db62a12d3b4
Nolan Hawkins <thatwhich@dijitalelefan.com>
2016-02-06 09:55:43 -0500

    added modal close button css

--- DOM/modal_window.js
+++ DOM/modal_window.js
@@ -20,3 +20,41 @@ function createModalWindow(innerHTML, elem){
         document.lastChild.lastChild.removeChild(modal);
     });
 }
+
+/*
+ * Should go with CSS like so: 
+ * 
+ * .close-button {
+ *     width: 1em;
+ *     height: 1em;
+ *     position: absolute;
+ *     right: 1em;
+ *     top: 1em;
+ *     overflow: hidden;
+ *     text-indent: 1em;
+ *     font-size: 0.75em;
+ *     border: none;
+ *     background: transparent;
+ *     color: transparent;
+ *     cursor:pointer;
+ * }
+ * .close-button::before,
+ * .close-button::after {
+ *     content: '';
+ *     position: absolute;
+ *     width: 3px;
+ *     height: 100%;
+ *     top: 0;
+ *     left: 50%;
+ *     background: #bdc3c7;
+ * }
+ * .close-button::before {
+ *     -webkit-transform: rotate(45deg);
+ *     transform: rotate(45deg);
+ * }
+ * .close-button::after {
+ *     -webkit-transform: rotate(-45deg);
+ *     transform: rotate(-45deg);
+ * }
+ * 
+ */
